51d0e5cb5e79ff4543638ac308c289f13bb63f2a
Carsten Knoll <carsten.knoll@posteo.de>
2018-09-09 18:44:11 +0200

    new mechanism to handle site specific settings

--- sober_site/sober_site/settings.py
+++ sober_site/sober_site/settings.py
@@ -20,12 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1234567890abcdefghijklmnopqrstuvwxyz1234567890abcd'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", os.environ.get("THIS_SERVER_IP")]
+# that file must be changed for deployment and kept secret
+from .site_specific_settings import SECRET_KEY, DEBUG, ALLOWED_HOSTS
 
 
 # Application definition
---
+++ sober_site/sober_site/site_specific_settings.py
@@ -0,0 +1,18 @@
+"""
+This file contains information which is inherently specific to each instance.
+It should not be shared (e.g. in a git repo).
+
+The values here are dummy values (which nevertheless should work)
+
+The variables defined here are imported to settings.py
+
+"""
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '1234567890abcdefghijklmnopqrstuvwxyz1234567890abcd'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["127.0.0.1"]
+
